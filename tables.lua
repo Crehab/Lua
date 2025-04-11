42852f47792773d3bc7c6fed4de9ec8a221d4bc3
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-11 17:49:25 +0530

    update Table file  with examples of assigned values and nil values

--- tables.lua
+++ tables.lua
@@ -3,3 +3,25 @@ used to store sets of related data since the data is stored in the same place, i
 organize, store and maintain the data.]]
 
 
+currentLevel = 0
+
+totalLevels = {3} -- total size of the Table
+
+totalLevels[1] = 1
+totalLevels[2] = 2
+totalLevels[3] = 3
+
+
+currentLevel = totalLevels[1]
+currentLevel = totalLevels[2]
+currentLevel = totalLevels[3]
+
+print("you are in level:",currentLevel)
+print("you are in level:",currentLevel)
+print("you are in final level",currentLevel)
+
+-- what if we put the index 4, that is empty and assigned nothing to it ?
+-- we will get nil, means nothing
+
+currentLevel = totalLevels[4]
+print("this is representation of nil value:",currentLevel)
