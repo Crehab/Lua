e4527fbe8a06edb54f00d46cb25c8d7d108fdc5d
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-11 17:39:43 +0530

    add Table Definition

---
+++ tables.lua
@@ -0,0 +1,5 @@
+--[[Tables are Data Structures which stores multiple amount of data in the same variables like, integers, strings etc..
+used to store sets of related data since the data is stored in the same place, its easy to
+organize, store and maintain the data.]]
+
+
