f01beecbf67126190787d5c3f21b4c01ed14ffbe
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-12 15:54:34 +0530

    add second type of function

--- moreFunctions.lua
+++ moreFunctions.lua
@@ -1,5 +1,16 @@
-local function Hello(name)
+--Method 1
+
+local function Hello(name) 
   print("Hello !",name)
 end
 
-Hello("Rohan")
+Hello("Rohan") 
+
+
+
+--[[ Method 2 ]]
+local greet = function(name)
+  print("Greetings,".. name .."!")
+end 
+
+greet("rohan") 
