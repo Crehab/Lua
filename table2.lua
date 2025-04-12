e2535b337155b2b452ea25a046e099489af95b3d
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-12 10:12:52 +0530

    different ways of printing and making a table.

---
+++ table2.lua
@@ -0,0 +1,13 @@
+-- Different Ways of Printing the Table in Lua
+
+-- Method 1
+
+table  = {} -- dynamic table, this table is not limited, you can add unlimited number of values in it.(maybe)
+
+
+table [1] = 1
+table [2] = 2
+print("the value at table1 is:",table[1])
+print("the value at table2 is:",table[2])
+-- it will print out the value at the 1st index.
+
